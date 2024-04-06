a6373e0aac9422ca5136e88e7a6d6caf5d8a4462
justinroche <justinroche03@gmail.com>
2024-04-05 23:43:06 -0500

    Outline algorithm

--- MD5PasswordProject/Program.cs
+++ MD5PasswordProject/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MD5PasswordProject
+{
+
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+
+      // Get keywords from txt file.
+      // Generate possible passwords from set of keywords.
+      // Check for correct formatting.
+      // Hash valid passwords and check against stolen hash.
+      // Print and store incorrect passwords and hashes to txt file.
+      // Confirm valid password and hash and break from algorithm.
+
+    }
+
+    static void CheckHash()
+
+  }
+
+}
